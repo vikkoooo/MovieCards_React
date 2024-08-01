75a2ed1ddd36e1aeb4b30cd450f74cc7afbc0c77
vikkoooo <vikkolundberg@gmail.com>
2024-08-01 14:20:00 +0200

    create moviecard with props

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -1,8 +1,19 @@
 import { ReactElement } from "react";
 
-export function MovieCard(): ReactElement {
+interface MovieCardProps {
+	title: string;
+	rating: number;
+	genre: string;
+	description: string;
+}
+
+export function MovieCard(prop: MovieCardProps): ReactElement {
 	return (
 		<article className="movie-card">
+			<h1 className="movie-title">{prop.title}</h1>
+			<h2 className="movie-rating">{prop.rating}</h2>
+			<h2 className="movie-genre">{prop.genre}</h2>
+			<p className="movie-description">{prop.description}</p>
 		</article>
 	);
 }
