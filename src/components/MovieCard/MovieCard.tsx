e488c38b3dafb0006117c7de46a180318a9c746f
vikkoooo <vikkolundberg@gmail.com>
2024-08-02 15:21:08 +0200

    more moviecard styling

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -10,10 +10,22 @@ interface IMovieCardProps {
 export function MovieCard(prop: IMovieCardProps): ReactElement {
 	return (
 		<article className="movie-card" onClick={prop.onDeleteClicked}>
-			<h1 className="movie-title">{prop.movieData.title}</h1>
-			<h2 className="movie-rating">{prop.movieData.rating}</h2>
-			<h2 className="movie-genre">{prop.movieData.genre}</h2>
-			<p className="movie-description">{prop.movieData.description}</p>
+			<div className="row-container">
+				<p className="heading">Title:</p>
+				<h1 className="movie-title">{prop.movieData.title}</h1>
+			</div>
+			<div className="row-container">
+				<p className="heading">Rating:</p>
+				<h2 className="movie-rating">{prop.movieData.rating}</h2>
+			</div>
+			<div className="row-container">
+				<p className="heading">Genre:</p>
+				<h2 className="movie-genre">{prop.movieData.genre}</h2>
+			</div>
+			<div className="row-container">
+				<p className="heading">Description:</p>
+				<p className="movie-description">{prop.movieData.description}</p>
+			</div>
 		</article>
 	);
 }
